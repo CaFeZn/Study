91a20b476ecfa85bf41dab3d5579f082d5f39f1c
CaFeZn <1837781998@qq.com>
2022-09-27 19:40:53 +0800

    Create TenToTwo.c

---
+++ TenToTwo.c
@@ -0,0 +1,22 @@
+﻿//十进制转二进制
+
+#include <stdio.h>
+
+int main()
+{
+	int ten;//十进制数//
+	scanf("%d", &ten);
+	int times = 1;//倍数//
+	int two = 0;//二进制数//
+	for (;ten != 0;)
+	{
+		//逐位输出二进制数//
+		two = two + ten % 2 * times;
+		//准备下一次计算除二取余//
+		ten = (ten / 2);
+		times = times * 10;
+	}
+	
+	printf("%d\n", two);
+	return 0;
+}
